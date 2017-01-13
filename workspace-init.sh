e2170c700a7d29f6105ce52a63dec7f51e174340
Igor Jovic <jovic.i24@gmail.com>
2017-01-13 21:50:29 +0100

    Update workspace-init.sh

--- workspace-init.sh
+++ workspace-init.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 # This script creates nested git repository in dms-workspace folder in the directory where it is ran
-# Run as Administrato
-
-echo "======================================="
-echo "Creating directory dms-workspace"
-mkdir dms-workspace
-cd dms-workspace
-
-echo "======================================="
-echo "Cloning DocumentManagementSystem-Backend into dms-workspace"
-cd dms-workspace
-git clone https://github.com/Igor-Jovic/DocumentManagementSystem-Backend.git 
-
+# Run as Administrator
 echo "======================================="
 echo "Cloning DocumentManagementSystem-FrontEnd into DocumentManagementSystem-Backend/src/main/resources/static"
 cd DocumentManagementSystem-Backend/src/main/resources
