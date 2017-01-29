694c0ad14bde587aed379ae8214871ed215b681c
ssstefan <stefan93ss@hotmail.com>
2017-01-29 14:03:14 +0100

    workspace-init.sh delete static folder

--- workspace-init.sh
+++ workspace-init.sh
@@ -4,6 +4,7 @@
 echo "======================================="
 echo "Cloning DocumentManagementSystem-FrontEnd into DocumentManagementSystem-Backend/src/main/resources/static"
 cd src/main/resources
+rm -rf static
 git clone https://github.com/Igor-Jovic/DocumentManagementSystem-Frontend.git static
 
 echo "======================================="
