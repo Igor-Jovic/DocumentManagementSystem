bd3f2a156aa68e4c56d1632e1e3fc3e51fae4b9c
= <jovic.i24@gmail.com>
2017-02-01 20:16:17 +0100

    remove unnecessary system out printlns

--- src/main/java/rs/ac/bg/fon/silab/dms/security/SecurityFilter.java
+++ src/main/java/rs/ac/bg/fon/silab/dms/security/SecurityFilter.java
@@ -16,10 +16,8 @@ public class SecurityFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
         String token = httpServletRequest.getHeader(AUTHORIZATION_HEADER);
-        System.out.println("TOKEN: " + token);
         AuthenticationData authenticationData = tokenAuthenticationService.getAuthenticationData(token);
         if (authenticationData != null) {
-            System.out.println("access allowed");
             SecurityContextHolder.getContext().setAuthentication(authenticationData.getAuthentication());
         }
         filterChain.doFilter(httpServletRequest, httpServletResponse);
