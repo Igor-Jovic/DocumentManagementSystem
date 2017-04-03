c628261877bb792abffb767f0c1bf6267864e83c
TAAJOIG1 <igor.jovic@swisscom.com>
2017-04-03 15:33:40 +0200

    process service test

--- src/test/java/rs/ac/bg/fon/silab/dms/core/service/ProcessServiceTest.java
+++ src/test/java/rs/ac/bg/fon/silab/dms/core/service/ProcessServiceTest.java
@@ -6,26 +6,43 @@
 package rs.ac.bg.fon.silab.dms.core.service;
 
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+
 import rs.ac.bg.fon.silab.dms.core.exception.DMSErrorException;
+import rs.ac.bg.fon.silab.dms.core.model.CompanyProcess;
 import rs.ac.bg.fon.silab.dms.core.repository.ProcessRepository;
 
 /**
- *
  * @author stefan
  */
 public class ProcessServiceTest {
 
     @Test(expected = DMSErrorException.class)
     public void getProcess_processNotExists_throwBadRequestException() throws DMSErrorException {
-        Long processId = 12l;
+        Long processId = 12L;
         ProcessRepository processRepositoryMock = mock(ProcessRepository.class);
-        
+
         when(processRepositoryMock.findOne(processId)).thenReturn(null);
-        
+
         ProcessService testee = new ProcessService(processRepositoryMock);
         testee.getProcess(processId);
     }
-    
+
+    @Test
+    public void getProcess_processExists_returnsIt() throws DMSErrorException {
+        Long processId = 12L;
+        ProcessRepository processRepositoryMock = mock(ProcessRepository.class);
+
+        CompanyProcess process = new CompanyProcess();
+        when(processRepositoryMock.findOne(processId)).thenReturn(process);
+
+        ProcessService testee = new ProcessService(processRepositoryMock);
+        CompanyProcess existingProcess = testee.getProcess(processId);
+
+        assertEquals(process, existingProcess);
+    }
+
 }
