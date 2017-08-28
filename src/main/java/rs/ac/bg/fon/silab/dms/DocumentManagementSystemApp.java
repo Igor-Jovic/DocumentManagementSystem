5c7004df2dfb37f3f916b7dd30c67e5282daca76
ssstefan <stefan93ss@hotmail.com>
2017-08-28 23:10:07 +0200

    use mysql for dev environment

--- src/main/java/rs/ac/bg/fon/silab/dms/DocumentManagementSystemApp.java
+++ src/main/java/rs/ac/bg/fon/silab/dms/DocumentManagementSystemApp.java
@@ -1,10 +1,12 @@
 package rs.ac.bg.fon.silab.dms;
 
+import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.transaction.annotation.Transactional;
 import rs.ac.bg.fon.silab.dms.core.model.Company;
 import rs.ac.bg.fon.silab.dms.core.model.Role;
@@ -28,21 +30,26 @@ public class DocumentManagementSystemApp {
             @Autowired
             ProcessRepository processRepository;
 
+            @Autowired
+            private Environment environment;
+
             @Override
             @Transactional
             public void run(String... strings) throws Exception {
-                User u = new User();
-                u.setUsername("admin");
-                u.setPassword("admin");
-                u.setCompany(new Company("adminCompany"));
-                u.setRole(Role.ADMIN);
-                User admin = userService.createUserWithNewCompany(u);
-                User u2 = new User();
-                u2.setUsername("notAdmin");
-                u2.setPassword("notAdmin");
-                u2.setRole(Role.USER);
-                u2.setCompany(admin.getCompany());
-                userService.createUser(u2);
+                if (Arrays.asList(environment.getActiveProfiles()).contains("test")) {
+                    User u = new User();
+                    u.setUsername("admin");
+                    u.setPassword("admin");
+                    u.setCompany(new Company("adminCompany"));
+                    u.setRole(Role.ADMIN);
+                    User admin = userService.createUserWithNewCompany(u);
+                    User u2 = new User();
+                    u2.setUsername("notAdmin");
+                    u2.setPassword("notAdmin");
+                    u2.setRole(Role.USER);
+                    u2.setCompany(admin.getCompany());
+                    userService.createUser(u2);
+                }
             }
         };
     }
