7153dcbc90fdc2b7978aff5f032a192664b8b32b
ssstefan <stefan93ss@hotmail.com>
2017-08-28 23:42:51 +0200

    updateCompany; get company by name or description w/ elasticsearchTemplate and SearchQuery

--- src/main/java/rs/ac/bg/fon/silab/dms/core/service/CompanyService.java
+++ src/main/java/rs/ac/bg/fon/silab/dms/core/service/CompanyService.java
@@ -1,8 +1,15 @@
 package rs.ac.bg.fon.silab.dms.core.service;
 
 import java.util.List;
+import org.elasticsearch.action.search.SearchType;
+import org.elasticsearch.common.unit.Fuzziness;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
+import org.springframework.data.elasticsearch.core.query.SearchQuery;
 import org.springframework.stereotype.Service;
 import rs.ac.bg.fon.silab.dms.core.exception.DMSErrorException;
 import rs.ac.bg.fon.silab.dms.core.model.Company;
@@ -18,6 +25,9 @@ public class CompanyService {
 
     @Autowired
     private CompanyESRepository companyESRepository;
+
+    @Autowired
+    private ElasticsearchTemplate elasticsearchTemplate;
 
     CompanyService(CompanyRepository companyRepository, CompanyESRepository companyESRepository) {
         this.companyRepository = companyRepository;
@@ -35,6 +45,23 @@ public class CompanyService {
 
     public List<CompanyES> getByNameLike(String name, Pageable pageable) {
         return companyESRepository.findByNameLike(name, pageable).getContent();
+    }
+
+    public void updateCompany(Company company) {
+        companyRepository.save(company);
+        companyESRepository.save(new CompanyES(company));
+    }
+
+    public List<CompanyES> getByNameOrDescription(String searchExpression, Pageable pageable) {
+        SearchQuery searchQuery = new NativeSearchQueryBuilder()
+                .withQuery(new BoolQueryBuilder().should(QueryBuilders.matchPhrasePrefixQuery("name", searchExpression))
+                        .should(QueryBuilders.matchQuery("description", searchExpression).fuzziness(Fuzziness.TWO)))
+                .withPageable(pageable)
+                .withIndices("dms")
+                .withTypes("company")
+                .withSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+                .build();
+        return elasticsearchTemplate.queryForPage(searchQuery, CompanyES.class).getContent();
     }
 
 }
